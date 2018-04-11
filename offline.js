76e54412f4887a58328da7fbc9e44d57c20e5c5f
Pierre-Alain BESSERO <pierrealain.bessero@etudiant.univ-lr.fr>
2018-04-11 16:11:27 +0200

    First Demo
    Created and added the menu and the splash screen

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1523455794,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/startpage_logo_background-sheet0.png",
+		"images/startpage_logo2-sheet0.png",
+		"images/startpage_logo-sheet0.png",
+		"images/startpage_logo_bar-sheet0.png",
+		"images/startpagepin-sheet0.png",
+		"images/picture-sheet0.png",
+		"images/move-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
